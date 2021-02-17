52f5c751aebeeef95e9e6c845b9033b328046570
Artur Bespalyy <zkhomers@gmail.com>
2021-02-17 19:14:55 +0200

    calendar tamplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    plaseholder:"hello calendar",
+  }
+  render() {
+
+   return (
+     <>
+       <h1>{this.state.plaseholder}</h1>
+
+     </>
+
+   )
+  }
 }
 
 export default App;
